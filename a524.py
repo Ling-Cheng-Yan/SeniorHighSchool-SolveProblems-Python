a59bec6ebd296b1914c139d10a1dd16730d8a455
Ling-Cheng-Yan <darkness851106@gmail.com>
2020-09-23 23:56:02 +0800

    a524-second_commit

--- a524.py
+++ a524.py
@@ -1,24 +1,25 @@
-from itertools import permutations
-try:
-    while True:
-        lst1 = list()
-        lst2 = list()
-        str1 = str()
-        ans = int(input())
-        ans = ans + 1
-        for i in range(ans):
-            lst1.append(i)
-        del lst1[0]
-        lst2 = list(permutations(lst1))
-        print(lst2)
-        length = len(lst2[0])
-        for i in lst2:
-            for j in range(length):
-                for z in i:
-                    str1 = str1 + str(z)
-                print(str1)
+import sys
 
+def recursion(lt,use,current,n):
+    if current==n:
+        for i in range(n):
+            print("{0}".format(lt[i]),end="")
+        print()
+    else:
+        for i in range(n-1,-1,-1):
+            if use[i]==0 :
+                use[i]=1
+                lt[current]=i+1
+                recursion(lt,use,current+1,n)
+                use[i]=0
+            else:
+                pass
 
-except EOFError:
-    pass
+lt=[0]*8
+use=[0]*8
 
+for n in sys.stdin:
+    if n[0]!="\n":
+        n=int(n)
+        use=[0]*n
+        recursion(lt,use,0,n)
